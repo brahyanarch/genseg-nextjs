69d9d94ed70de866a7290ac12b0a50a86c952212
berning <bernardpari2002@gmail.com>
2024-11-19 09:50:12 -0500

    Mejorando los Avisos

---
+++ src/context/avisoContext.tsx
@@ -0,0 +1,48 @@
+// AvisoContext.tsx
+'use client';
+import React, { createContext, useState,ReactNode } from 'react';
+import clsx from 'clsx'
+import Aviso from '@/components/aviso';
+
+
+interface AvisoContextProps {
+  mostrarAviso: (type: string, content: string, fixed: boolean) => void;
+}
+
+export const AvisoContext = createContext<AvisoContextProps | undefined>(undefined);
+
+const AvisoProvider = ({ children }: { children: ReactNode }) => {
+  const [avisoData, setAvisoData] = useState<{ type: string; content: string; fixed: boolean } | null>(null);
+  const [isVisible, setIsVisible] = useState(false);
+
+  const mostrarAviso = (type: string, content: string, fixed: boolean = false) => {
+    setAvisoData({ type, content, fixed });
+    setIsVisible(true);
+
+    if (!fixed) {
+      setTimeout(() => {
+        setIsVisible(false);
+      }, 3000); // Ocultar después de 3 segundos
+    }else {
+        setIsVisible(true);
+    }
+  };
+
+  return (
+    <AvisoContext.Provider value={{ mostrarAviso }}>
+      {children}
+      {isVisible && avisoData && (
+        <div className={clsx("fixed top-4 left-1/2 transform -translate-x-1/2",{
+          "w-[80%]": avisoData.fixed == true,
+          "w-[20%]": avisoData.fixed === true,
+
+        })}>
+          <Aviso type={avisoData.type} content={avisoData.content} fixed={avisoData.fixed} />
+        </div>
+      )}
+    </AvisoContext.Provider>
+  );
+};
+
+
+export default AvisoProvider;
